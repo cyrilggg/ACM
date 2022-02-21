a87dfb8f4f9e980b344809bc723dc681d026ccaa
cyrilggg <344327193@qq.com>
2022-02-21 20:42:10 +0800

    2022.2.21

---
+++ problem/Atcoder/ABC/240/D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int N = 2e5 + 10;
+int a[N], cnt[N];
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int cnt = 0;
+    bool flag = false;
+    for (int i = 1; i <= n; i ++ )  cin >> a[i];
+    for (int i = 1, j = 2; i <= n; i ++ )
+    {
+        if (!flag)
+        {
+            
+        }
+    }
+}
+
+signed main()
+{
+   int t = 1;
+   //cin >> t;
+   while (t -- )solve();
+}
